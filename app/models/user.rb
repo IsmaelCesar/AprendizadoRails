70b3fe48dc8416a3897e9ee3412921233b01bf8d
IsmaelCesar <leamscesar@gmail.com>
2020-10-02 08:04:41 -0300

    Addding Basic implementatio modules for the application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  VALID_USERNAME_FORMAT = /[a-zA-Z]+[0-9]*/
+  validates name, format: { with: VALID_USERNAME_FORMAT }, uniqueness: { case_sensitive: true }, presence: true
+  validates password, presence: true
+  before_save { self.name = name }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_150056) do
+ActiveRecord::Schema.define(version: 2020_10_01_161345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_10_01_150056) do
     t.decimal "money"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["name"], name: "index_users_on_name"
   end
 
   create_table "wallets", force: :cascade do |t|
