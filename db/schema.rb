6da561d62cb1940412c248fa4aad91376653f246
Ismael Cesar <leamscesar@gmail.com>
2020-10-02 16:43:58 -0300

    Adding Price Column to coins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_120623) do
+ActiveRecord::Schema.define(version: 2020_10_02_193659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_120623) do
     t.datetime "updated_at", null: false
     t.decimal "value"
     t.string "url_image"
+    t.decimal "price"
     t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
   end
 
