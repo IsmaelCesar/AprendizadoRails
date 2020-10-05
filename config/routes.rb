9170a75bd9542655c0fa7403a7febbefb526f60d
IsmaelCesar <leamscesar@gmail.com>
2020-10-05 10:17:37 -0300

    Adding custom routes to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # get 'user/new'
-  # get 'user/create'
-  # get 'user/edit'
-  # get 'user/update'
-  # get 'user/destroy'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'sign_out', to: 'sessions#destroy'
+
   resource :users
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
